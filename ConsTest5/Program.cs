9706e9e98d0f56de56c32de947537c0afd4e7ff0
RAVI KUMAR <rk1403307@outlook.com>
2020-08-11 18:01:47 +0530

    Commited test5

---
+++ ConsTest5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsTest5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C program to find the largest of three numbers.
+            int a, b, c;
+            Console.WriteLine("Enter the Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a>b && a>c)
+            {
+                Console.WriteLine(a + "is largest Number");
+            }
+            else if(b>a && b>c)
+            {
+                Console.WriteLine(b + "is largest Number");
+            }
+            else
+            {
+                Console.WriteLine(c + "is largest Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
