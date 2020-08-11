cb5a478ab1f77ccc6795e3cfbe73f5efb476bc0b
RAVI KUMAR <rk1403307@outlook.com>
2020-08-11 23:23:39 +0530

    Multiple class

---
+++ ConsoleTest1/ConsTest7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest1
+{
+    public class ConsTest7
+    {
+        //WAP in C to display n terms of natural number and their sum.
+        public int NaturalNo(int n)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the Number.");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i=1; i<=n; i++)
+            {
+                Console.WriteLine(i);
+                sum = sum + i;
+            }
+            return sum;
+
+        }
+    }
+}
