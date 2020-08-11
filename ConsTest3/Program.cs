475840995d66852533d43385fbf109d22ab83057
RAVI KUMAR <rk1403307@outlook.com>
2020-08-11 21:59:08 +0530

    Test3 commited

---
+++ ConsTest3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WAP to check two given integers, and return true if one of them is 30 or if their sum is 30.
+             int a, b;
+            
+            Console.WriteLine("Enter the Number.");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number.");
+            b = Convert.ToInt32(Console.ReadLine());
+            if(a==30 || b==30 ||(a+b)==30)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadLine();
+        }
+    }
+}
