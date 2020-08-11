1b5943680fd1b2d914440d387ca88f3d9760a466
RAVI KUMAR <rk1403307@outlook.com>
2020-08-11 22:11:56 +0530

    Triple Absolute Difference

---
+++ ConTest2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WAP to get the absolute difference between n and 51.If n is greater than 51 return triple the absolute difference.
+            int n, m = 51,sub;
+            Console.WriteLine("Enter the Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            sub = (n - m);
+            if (n>51)
+            {
+                Console.WriteLine(sub * 3 + " is triple the absolute difference.");
+            }
+            else
+            {
+                Console.WriteLine(sub + "is only absolute difference.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
