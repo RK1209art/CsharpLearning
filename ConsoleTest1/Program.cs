ecaa6f7dbb6cf53997704a11061e0c83d7c13f46
RAVI KUMAR <rk1403307@outlook.com>
2020-08-11 22:14:34 +0530

    Largest Number b/w two

---
+++ ConsoleTest1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //WAP to find the largest Number b/w two.
+            int num1, num2;
+            Console.WriteLine("Enter the Number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number.");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + "is the Largest Number");
+            }
+            else
+            {
+                Console.WriteLine(num2 + "is the Largest Number");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
